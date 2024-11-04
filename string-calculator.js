25e3ab9716c66fe91a6a66b7d8c0c45244d797f7
unknown <milansanadhya1996@gmail.com>
2024-11-04 15:02:58 +0530

    Add support for newline-delimited strings

--- string-calculator.js
+++ string-calculator.js
@@ -1,6 +1,12 @@
 export const add = (string) => {
   if (string === "") return 0;
 
-  const result = string.split(",").reduce((sum, number) => sum + +number, 0);
+  const numbers = string
+    .split(/[,\n]/)
+    .map((str) => +str)
+    .filter((num) => !isNaN(num));
+  const result = numbers.reduce((acc, cur) => {
+    return acc + cur;
+  }, 0);
   return result;
 };
--- string-calculator.test.js
+++ string-calculator.test.js
@@ -16,4 +16,8 @@ describe("string-calculator", () => {
     const number = add("5,6,8,9,7");
     expect(number).toBe(35);
   });
+  test("string contains new line delimeter", () => {
+    const number = add("1\n2,3");
+    expect(number).toBe(6);
+  });
 });
